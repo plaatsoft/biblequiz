ecd05a53b4ff5367ed2e3c610525924ec6806e55
wplaat <wplaat@81db75ae-d6cc-11de-b13e-7d68ceb8f08a>
2010-01-25 20:38:03 +0000

    Added more sourcecode documentation based on Doxygen

--- trunk/include/general.h
+++ trunk/include/general.h
@@ -1,22 +1,33 @@
-/* 
-**  bibleQuiz for Wii - Created by wplaat (www.plaatsoft.nl) 
-**
-**  Copyright (C) 2008-2009
-**  =======================
-**
-**  This program is free software; you can redistribute it and/or modify
-**  it under the terms of the GNU General Public License as published by
-**  the Free Software Foundation, version 2.
-**
-**  This program is distributed in the hope that it will be useful,
-**  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**  GNU General Public License for more details.
-**
-**  You should have received a copy of the GNU General Public License
-**  along with this program; if not, write to the Free Software
-**  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+/**
+ * @mainpage Wii BibleQuiz Documentation
+ * Welcome to the BibleQuiz documentation.
+ *
+ * @section Introduction
+ * BibleQuiz is a classic bible quiz game for the Nintedo Wii.
+ *
+ * @section Links
+ * Website: http://www.plaatsoft.nl\n
+ * Code: http://code.google.com/p/biblequiz\n
+ *
+ * @section Credits
+ * Documentation  : wplaat\n
+ *
+ * @section Licence
+ * Copyright (c) 2008-2010 Plaatsoft
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 2.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include "GRRLIB.h"
 
